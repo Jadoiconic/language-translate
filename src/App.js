deb698eb58ea13d62f488731ffd80450b4bf3366
Nizeyimana Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-09-04 23:04:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 
-function App() {
+const lang = [
+  {
+  code:'en',
+  name:'English',
+  countryCode:'bg',
+},
+  {
+  code:'fr',
+  name:'Frencais',
+  countryCode:'fr',
+},
+]
+const App = () => {
+  const { t } = useTranslation();
+
+
+  useEffect(()=>{
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="d-flex py-2 align-items-center shadow justify-content-between px-4">
+        <div>
+          <h1>{t('welcomeToReact')}</h1>
+        </div>
+        <div className="dropdown">
+          <button
+            className="btn dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {t('language')}
+          </button>
+          <ul className="dropdown-menu dropdown-menu">
+            { lang.map(({name,code,countryCode})=> 
+              <li>
+              <button className="dropdown-item" onClick={()=>i18next.changeLanguage(code)}>
+                <span className="flag-icon flag-icon-bg mx-2"> </span>
+                {name}
+              </button>
+            </li>
+            )
+            }
+            
+          </ul>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="">
+          
+          <h2>{t("deleteAccount")}</h2>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: "en",
+    detection: {
+      order: [ "htmlTag","cookie", "localStorage", "path", "subdomain"],
+      caches: ["cookie", "localStorage"],
+    },
+    backend: {
+      loadPath: "assets/locales/{{lng}}/translations.json",
+    },
+    React: { useSuspense: false },
+    interpolation: {
+      escapeValue: false, 
+    },
+  });
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
