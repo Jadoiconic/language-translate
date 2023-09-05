6c75367a2b85509e7fada6fbb99a4ff7aaa3beba
Nizeyimana Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-09-05 08:25:33 +0200

    DevSolutions

--- src/App.js
+++ src/App.js
@@ -4,28 +4,26 @@ import i18next from "i18next";
 
 const lang = [
   {
-  code:'en',
-  name:'English',
-  countryCode:'bg',
-},
+    code: "en",
+    name: "English",
+    countryCode: "bg",
+  },
   {
-  code:'fr',
-  name:'Frencais',
-  countryCode:'fr',
-},
-]
+    code: "fr",
+    name: "Frencais",
+    countryCode: "fr",
+  },
+];
 const App = () => {
   const { t } = useTranslation();
 
-
-  useEffect(()=>{
-  },[])
+  useEffect(() => {}, []);
 
   return (
     <>
-      <nav className="d-flex py-2 align-items-center shadow justify-content-between px-4">
+      <nav className="d-flex py-2 align-items-center shadow justify-content-between px-4" style={{backgroundColor:'teal'}}>
         <div>
-          <h1>{t('welcomeToReact')}</h1>
+          <h1>{t("welcomeToReact")}</h1>
         </div>
         <div className="dropdown">
           <button
@@ -34,28 +32,37 @@ const App = () => {
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
-            {t('language')}
+            Change language
           </button>
           <ul className="dropdown-menu dropdown-menu">
-            { lang.map(({name,code,countryCode})=> 
-              <li>
-              <button className="dropdown-item" onClick={()=>i18next.changeLanguage(code)}>
-                <span className="flag-icon flag-icon-bg mx-2"> </span>
-                {name}
-              </button>
+            <li className="text-center">
+              <strong>{t("language")}</strong>
             </li>
-            )
-            }
-            
+            {lang.map(({ name, code, countryCode }) => (
+              <li>
+                <button
+                  className="dropdown-item"
+                  onClick={() => i18next.changeLanguage(code)}
+                >
+                  <span className="flag-icon flag-icon-bg mx-2"> </span>
+                  {name}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
-      <div className="container-fluid">
-        <div className="">
-          
-          <h2>{t("deleteAccount")}</h2>
-        </div>
+      <div
+        className="d-flex align-items-center"
+        style={{ minHeight: "80vh",backgroundColor:'#eee' }}
+      >
+        <article className="p-4">
+          <h2 className="fs-1">DevSolutions</h2>
+          <p className="fs-3">{t("welcomeNote")}</p>
+        </article>
+        <div className="p-4">{t("welcomeNote")}</div>
       </div>
+      <div className=""></div>
     </>
   );
 };
